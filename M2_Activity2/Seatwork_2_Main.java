9b29e61b3e53b5fe6c44f642032a2bde12e21806
Maica Josette A. Azuelo <mjaazuelo@headoffice.corp.bpi.com.ph>
2025-12-09 16:54:28 +0800

    Add M2_Activity2 files

---
+++ M2_Activity2/Seatwork_2_Main.java
@@ -0,0 +1,15 @@
+package Seatwork_2;
+
+public class Seatwork_2_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Whiteboard wb = new Whiteboard();
+        int sum = wb.addNumbers(5, 3);  // call the method and store the result
+
+        System.out.println("The total is: " + String.valueOf(sum));
+    
+	}
+
+}
---
+++ M2_Activity2/Whiteboard.java
@@ -0,0 +1,10 @@
+package Seatwork_2;
+
+public class Whiteboard {
+
+    int addNumbers(int addend1, int addend2) {
+        int total = addend1 + addend2;  
+        return total;                
+    }
+
+}
